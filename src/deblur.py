769eb17cc7e875f37c8e24a2aafa7d0220c4dded
Maksim Surov <surov@arrival.com>
2022-04-14 21:17:08 +0300

    upt

--- src/deblur.py
+++ src/deblur.py
@@ -1,5 +1,6 @@
 from re import L
 from tensorflow.keras import layers, models, Input, models, optimizers, losses
+from tensorflow.nn.experimental import stateless_dropout
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
@@ -14,8 +15,8 @@ def prepare_dataset(impath):
 
     inputs = []
     outputs = []
-    for i in range(0, h - 15, 11):
-        for j in range(0, w - 15, 11):
+    for i in range(0, h - 15, 3):
+        for j in range(0, w - 15, 3):
             inputs += [blurred[i:i+16,j:j+16]]
             outputs += [src[i+7:i+9,j+7:j+9]]
             # print(inputs[-1].shape, outputs[-1].shape)
@@ -24,22 +25,43 @@ def prepare_dataset(impath):
     outputs = np.array(outputs, dtype=np.float32) / 255
     return inputs, outputs
 
+def deblur_1ch(model, src):
+    h,w = src.shape
+    samples = []
+    for i in range(0, h - 15, 2):
+        for j in range(0, w - 15, 2):
+            sample = src[i:i+16,j:j+16]
+            samples += [sample]
+
+    samples = np.array(samples, dtype=np.float32) / 255
+    results = model(samples)
+    results = np.array(results, dtype=np.float32)
+
+    dst = np.zeros((h, w), dtype=np.float32)
+    k = 0
+    for i in range(0, h-15, 2):
+        for j in range(0, w-15, 2):
+            dst[i+7:i+9,j+7:j+9] = results[k,:,:]
+            k += 1
+
+    dst = np.clip(dst * 255, 0, 255).astype(np.uint8)
+    return dst
+
+def deblur(model, im):
+    h,w,c = im.shape
+    dst = np.zeros(im.shape, dtype=np.float32)
+    for i in range(c):
+        dst[:,:,i] = deblur_1ch(model, im[:,:,i])
+    return dst
 
 def test():
-    if False and exists('data/model_deblur'):
+    if exists('data/model_deblur'):
         model = models.load_model('data/model_deblur')
     else:
-        # l = [
-        #     Input(shape=(16, 16, 1)),
-        #     layers.Conv2D(10, kernel_size=(3,3), activation='sigmoid'),
-        #     layers.Conv2D(10, kernel_size=(3,3), activation='sigmoid'),
-        #     layers.Flatten(),
-        #     layers.Dense(128, activation='sigmoid'),
-        #     layers.Dense(4, activation='sigmoid'),
-        #     layers.Reshape((2, 2))
-        # ]
         l = [
             Input(shape=(16, 16, 1)),
+            layers.Conv2D(8, kernel_size=(3,3), activation='sigmoid'),
+            layers.Conv2D(8, kernel_size=(3,3), activation='sigmoid'),
             layers.Flatten(),
             layers.Dense(128, activation='sigmoid'),
             layers.Dense(4, activation='sigmoid'),
@@ -52,18 +74,19 @@ def test():
         loss = losses.MeanSquaredError()
         model.compile('adam', loss)
 
-        inputs, outputs = prepare_dataset('data/samples/flower.jpg')
+        inputs, outputs = prepare_dataset('data/sample_deblur/flower.jpg')
         n = len(inputs)
         indices = np.arange(n, dtype=int)
         np.random.shuffle(indices)
-        i1 = indices[0:3*n//4]
-        i2 = indices[3*n//4:]
+
+        i1 = indices[0:n//5]
+        i2 = indices[n//5:n//5 + 100]
         training_inputs = inputs[i1]
         training_outputs = outputs[i1]
         test_inputs = inputs[i2]
         test_outputs = outputs[i2]
 
-        history = model.fit(training_inputs, training_outputs, epochs=100, 
+        history = model.fit(training_inputs, training_outputs, epochs=300, 
             validation_data=(test_inputs, test_outputs))
         model.save('data/model_deblur')
         plt.plot(history.history['loss'])
@@ -72,31 +95,11 @@ def test():
 
     if True:
         model = models.load_model('data/model_deblur')
-        src = cv2.imread('data/samples/flower.jpg', 0)
-        cv2.imwrite('data/src.png', src)
-        blurred = cv2.GaussianBlur(src, (5,5), 1)
-        cv2.imwrite('data/blurred.png', blurred)
-
-        h,w = blurred.shape
-        samples = []
-        for i in range(0, h - 15, 2):
-            for j in range(0, w - 15, 2):
-                sample = blurred[i:i+16,j:j+16]
-                samples += [sample]
-
-        samples = np.array(samples, dtype=np.float32) / 255
-        results = model(samples)
-        results = np.array(results, dtype=np.float32)
-
-        dst = np.zeros((h, w))
-        k = 0
-        for i in range(0, h-15, 2):
-            for j in range(0, w-15, 2):
-                dst[i+7:i+9,j+7:j+9] = results[k,:,:]
-                k += 1
-
-        dst = np.clip(dst * 255, 0, 255).astype(np.uint8)
-        cv2.imwrite('data/deblurred.png', dst)
+        src = cv2.imread('data/sample_deblur/flower-cr.jpg')
+        blurred = cv2.pyrUp(src)
+        cv2.imwrite('data/sample_deblur/blurred.jpg', blurred)
+        dst = deblur(model, blurred)
+        cv2.imwrite('data/sample_deblur/deblurred.jpg', dst)
 
 
 if __name__ == '__main__':
